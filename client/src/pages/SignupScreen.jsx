e2baa2f2da53870b410e99b3f65a7995abfafdda
pourya1434 <pourya.lotfy@outlook.com>
2023-09-21 15:58:20 +0330

    confirm password

--- client/src/pages/SignupScreen.jsx
+++ client/src/pages/SignupScreen.jsx
@@ -1,22 +1,35 @@
 import { LockClosedIcon } from "@heroicons/react/20/solid";
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 import { Link, useNavigate } from "react-router-dom";
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {registerUserAction} from '../redux/slice/users/userSlice';
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 
 export default function Signup() {
   const [formData, setFormData] = useState({
     email: "",
     password: ""
   })
+  const [matchpassword, setMatchpassword] = useState("")
   const dispatch = useDispatch()
   const navigator = useNavigate()
-
-  const submitHandler = (e) => {
+  const {userAuth, isLoading, error} = useSelector(state => state.user)
+//  console.log(userAuth)
+  
+  useEffect(() => {
+    if (Object.keys(userAuth.userInfo).length !== 0) {
+      navigator('/')
+    }
+  },[userAuth.userInfo])
+  
+ const submitHandler = (e) => {
     e.preventDefault();
-    console.log(formData);
-    dispatch(registerUserAction(formData));
-    navigator('/')
+    if (formData["password"] !== formData["confirmPassword"]) {
+      setMatchpassword("The passwords are not matched, try again")
+    } else {
+      dispatch(registerUserAction(formData));
+    }
   }
 
   const onChangeHandler = (e) => {
@@ -25,7 +38,8 @@ export default function Signup() {
 
   return (
     <>
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    {isLoading? (<Loader />) : error ? (<Message>{error}</Message>) : matchpassword ? (<Message>{matchpassword}</Message>) : (
+        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
             <Link to="/">
@@ -70,6 +84,21 @@ export default function Signup() {
                   onChange={onChangeHandler}
                 />
               </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Passwordour account
+                </label>
+                <input
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Confirm Password"
+                  onChange={onChangeHandler}
+                />
+              </div>
             </div>
 
             <div className="flex items-center justify-between">
@@ -106,6 +135,7 @@ export default function Signup() {
           </form>
         </div>
       </div>
+    ) }
     </>
   );
 }
